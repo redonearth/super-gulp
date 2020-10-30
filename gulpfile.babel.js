27929448af7bdb3c53d1accd6e229ca99dfcf30f
redonearth <wona23@gmail.com>
2020-10-30 23:16:05 +0900

    #1.1 Pug Task part Two

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,4 +1,5 @@
 import gulp from "gulp";
+import del from "del";
 import gulpPug from "gulp-pug";
 
 const routes = {
@@ -8,7 +9,13 @@ const routes = {
   }
 };
 
-export const pug = () =>
+const clean = () => del(["build"]);
+
+const pug = () =>
   gulp.src(routes.pug.src).pipe(gulpPug()).pipe(gulp.dest(routes.pug.dest));
 
-export const dev = gulp.series([pug]);
+const prepare = gulp.dest([clean]);
+
+const assets = gulp.series([pug]);
+
+export const dev = gulp.series([prepare, assets]);
