3e73c49aef5b55578c8a7d1df01e21500a04f1e6
redonearth <wona23@gmail.com>
2020-10-31 22:31:38 +0900

    Fix typo

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -14,7 +14,7 @@ const clean = () => del(["build"]);
 const pug = () =>
   gulp.src(routes.pug.src).pipe(gulpPug()).pipe(gulp.dest(routes.pug.dest));
 
-const prepare = gulp.dest([clean]);
+const prepare = gulp.series([clean]);
 
 const assets = gulp.series([pug]);
 
