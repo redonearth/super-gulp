979366fb6040e1ecb458e6087f79e0ebafd46c74
Redonearth <wona23@gmail.com>
2020-11-02 23:01:50 +0900

    #3.0 Image Optimization

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,12 +2,17 @@ import gulp from "gulp";
 import del from "del";
 import gulpPug from "gulp-pug";
 import ws from "gulp-webserver";
+import gulpImage from "gulp-image";
 
 const routes = {
   pug: {
     watch: "src/**/*.pug",
     src: "src/*.pug",
     dest: "build"
+  },
+  img: {
+    src: "src/img/*",
+    dest: "build/img"
   }
 };
 
@@ -16,14 +21,18 @@ const clean = () => del(["build"]);
 const webserver = () =>
   gulp.src("build").pipe(ws({ livereload: true, open: true }));
 
+const img = () =>
+  gulp.src(routes.img.src).pipe(gulpImage()).pipe(gulp.dest(routes.img.dest));
+
 const watch = () => {
   gulp.watch(routes.pug.watch, pug);
+  gulp.watch(routes.img.src, img);
 };
 
 const pug = () =>
   gulp.src(routes.pug.src).pipe(gulpPug()).pipe(gulp.dest(routes.pug.dest));
 
-const prepare = gulp.series([clean]);
+const prepare = gulp.series([clean, img]);
 
 const assets = gulp.series([pug]);
 
